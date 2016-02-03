02b06d1f9d6c5df2bf9bf0ad744f3bd0273863b5
carillem <cmend429@mtroyal.ca>
2016-02-02 21:01:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/watson/punwarz/Login.java
@@ -0,0 +1,56 @@
+package com.example.watson.punwarz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.TextView;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.*;
+import com.facebook.*;
+
+public class Login extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private TextView info;
+    private LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_login);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User id: " + loginResult.getAccessToken().getUserId() + "\n" +
+                "Auth token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                info.setText("Login failed.");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
