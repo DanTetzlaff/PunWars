b18cd66f54c80dfffd47329446589420da158a2b
DSiwek <Dsiwe577@mtroyal.ca>
2016-03-06 19:50:53 -0800

    ParseApplication creates and exists
    Create and Exist functions

--- app/src/main/java/com/example/watson/punwarz/ParseApplication.java
+++ app/src/main/java/com/example/watson/punwarz/ParseApplication.java
@@ -4,7 +4,10 @@ import android.app.Application;
 
 import com.parse.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 
 public class ParseApplication extends Application {
@@ -12,7 +15,7 @@ public class ParseApplication extends Application {
     private String userObjectID = null; //the active user
     private String tempLobbyObjectID = null; //since Parse queries can't actually return???
     private String tempPostObjectID = null;
-    private boolean exists = false;
+    private boolean exists = false; //all-purpose boolean for query results.
     private ParseObject activeObject = null;
 
     @Override
@@ -24,34 +27,39 @@ public class ParseApplication extends Application {
 
     }
 
-
+    //creates a new user object in Parse if the user has not previously used app.
     public void createNewUser(String facebookID, String displayName) {
         ParseObject newUser = new ParseObject("Users");
         newUser.put("UserID", facebookID);
         newUser.put("DisplayName", displayName);
+        newUser.put("Score", 0);
         userObjectID = newUser.getObjectId(); //This may have to be after the save??
         newUser.saveInBackground();
     }
 
     //creates a new lobby associated with the active user
-    public void createNewLobby(String lobbyPrompt, int daysActive){
+    public void createNewLobby(String lobbyPrompt, Date expiryDate){
         ParseObject newLobby = new ParseObject("Lobby");
         newLobby.put("Prompt", lobbyPrompt);
         newLobby.put("CreatorID", userObjectID);
+        newLobby.put("ExpiryDate", expiryDate);
+        newLobby.put("IsActive", true);
         newLobby.saveInBackground();
-
     }
 
+    //creates a new Post associated to a particular Lobby
     public void createNewPost(String creatorID, String lobbyID, String postText){
         ParseObject newPost = new ParseObject("Posts");
         newPost.put("UserID", userObjectID);
         newPost.put("LobbyID", lobbyID);
         newPost.put("Post", postText);
+        newPost.put("ExpiryDate", getParseObject("Lobby", lobbyID).getDate("ExpiryDate"));
+        newPost.put("IsActive", true); //may not need this?
         newPost.saveInBackground();
         String postObjectID = newPost.getObjectId(); //may have to move this before the save??
     }
 
-
+    //sets the userObjectID variable so that it does not have to be queried multiple times.
     private void setUserObjectID(final String FacebookID){
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Users");
         query.whereEqualTo("UserID", FacebookID);
@@ -86,10 +94,11 @@ public class ParseApplication extends Application {
         });
     }
 
-    private ParseObject getLobbyObject(String lobbyID){
+    //runs a query and returns a ParseObject for manipulation.
+    private ParseObject getParseObject(String tableName, String objectID){
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Lobby");
-        query.whereEqualTo("ObjectID", lobbyID);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery(tableName);
+        query.whereEqualTo("ObjectID", objectID);
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             public void done(ParseObject object, ParseException e) {
                 if (object == null) {
@@ -113,14 +122,36 @@ public class ParseApplication extends Application {
         return exists;
     }
 
+    //checks if a lobby with an existing prompt description already exists. currently not case sensitive
     public boolean doesLobbyExist(String description){
         clearTempVars();
         checkIfObjectExists("Lobby", "Prompt", description);
         return exists;
     }
 
-    public void doesPostExist(String post) {
+    //checks if a post within a lobby already exists. currently not case sensitive.
+    public boolean doesPostExist(String post, String lobbyID) {
+        clearTempVars();
         // Posts are specific to a lobby, so a check would need the associated lobby ID
+        ParseQuery<ParseObject> checkPost = ParseQuery.getQuery("Posts");
+        checkPost.whereEqualTo("Post", post);
+
+        ParseQuery<ParseObject> checkLobby = ParseQuery.getQuery("Posts");
+        checkLobby.whereEqualTo("LobbyID", lobbyID);
+
+        List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
+        queries.add(checkPost);
+        queries.add(checkLobby);
+
+        //TODO: figure out AND functionality for Parse Queries
+        ParseQuery<ParseObject> mainQuery = ParseQuery.or(queries);
+        mainQuery.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> results, ParseException e) {
+                if (results.isEmpty()){exists = false;}
+                else {exists = true;}
+            }
+        });
+        return exists;
     }
 
     public String getLobbyObjectID(){
